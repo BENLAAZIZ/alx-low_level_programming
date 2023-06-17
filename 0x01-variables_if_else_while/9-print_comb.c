4ee420a1f28ea2499a68ff47b7ff8f9d332bc9b1
BENLAAZIZ <benlaaziz.hamza@gmail.com>
2023-06-17 09:19:30 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print all single digit numbers of base 10.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
