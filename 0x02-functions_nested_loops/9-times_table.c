acd424ff7e1935c5452eb3132f49f7384dd4e3fd
BENLAAZIZ <benlaaziz.hamza@gmail.com>
2023-06-21 03:27:30 +0100

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,9 @@ void times_table(void)
 {
 	int x, y, z, u, d;
 
-	for (x =0; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		for (y =0; y <= 9; y++)
+		for (y = 0; y <= 9; y++)
 		{
 			z = x * y;
 			if (z > 9)
