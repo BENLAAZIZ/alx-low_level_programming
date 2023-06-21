8ace712a3b9adf2dbbaa5ea2ab7827a4e2679315
BENLAAZIZ <benlaaziz.hamza@gmail.com>
2023-06-21 01:09:38 +0100

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,12 @@
  * Return: void
  */
  
- void print_alphabet(void)
- {
-	 char c;
-
-	 for (c = 'a'; c <= 'z'; c++)
-	 {
-		 _putchar(c);
-	 }
-	 _putchar('\n');
- }
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
