f0ea161a5569f48be081ea0e08a67275ebe8b7cd
BENLAAZIZ <benlaaziz.hamza@gmail.com>
2023-06-21 02:06:29 +0100

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+
+/**
+ * _islower - Check for lowercase character
+ * @c: the character to be checked
+ * Return: 1 for alphabitic charachter or 0 for anything else
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 98) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
