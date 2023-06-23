92e22e740f5cf25ff54d0cd2bf41a02486e77863
BENLAAZIZ <benlaaziz.hamza@gmail.com>
2023-06-23 13:26:00 -0700

    0-main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Test fct for positive or negative
+ * Return: 0 to success
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
+
