7bde26aa82b534e585ceb50f636e0aa194b937f5
BENLAAZIZ <benlaaziz.hamza@gmail.com>
2023-06-26 15:11:08 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -18,15 +18,15 @@ int main(void)
 	while (sum < 2772)
 	{
 		password[index] = 33 + rand() % 94;
-		sum += password[index];
+		sum += password[index++];
 	}
 
 	password[index] = '\0';
 
 	if (sum != 2772)
 	{
-		diff_half1 = (sum - 2772) /2;
-		diff_half2 = (sum - 2772) /2;
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
 
 		if ((sum - 2772) % 2 != 0)
 			diff_half1++;
